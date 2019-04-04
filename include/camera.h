9c06d1a403d79a9a73339d4157b37d1c9782276a
JoMedeiros <josivan.medeiros@live.com>
2019-04-04 15:38:34 -0300

    from smartphone

--- include/camera.h
+++ include/camera.h
@@ -1,17 +1,21 @@
 #ifndef _CAMERA_H_
 #define _CAMERA_H_
 
+#include "vec3.h"
+
 class Camera
 {
  private:
-  int height;
-  int width;
+  int _height;
+  int _width;
   Point3 origin;
   Point3 lower_left_corner;
   vec3 horizontal, vertical;
   vec3 u, v, w;
  public:
-  Camera(int _h, int _w) : height(_h),width(_w){}
-}
+  Camera(int _h, int _w) : _height(_h),_width(_w){}
+  int height() { return _height; }
+  int width() { return _width; }
+};
 
 #endif // _CAMERA_H_
